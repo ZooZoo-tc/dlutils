40815baaa88641ba87727aea56479e8930f80085
satish4github <sathishkumar0017@gmail.com>
2019-09-09 10:13:01 +0530

    Add files via upload

--- tfrecords.py
+++ tfrecords.py
@@ -74,58 +74,15 @@ class TFRecord:
         )
         writer.write(example.SerializeToString())
 
-  # def parser_v1(record,h=32,w=32,c=3):
-  #   dtype=tf.float32
-  #   features = {'image': tf.FixedLenFeature([h * w * c], dtype),
-  #                'label': tf.FixedLenFeature([], tf.int64)}
-  #   example = tf.io.parse_single_example(record, features=features)
-  #   x, y = example["image"], example['label']
-  #   x = tf.reshape(x, [h, w, c])
-  #   y = tf.one_hot(tf.cast(y, tf.int32), num_classes)
-  #   return x, y
-
-  def parser_v1(self,proto,num_classes=10):
-    # define your tfrecord again. Remember that you saved your image as a string.
-    # keys_to_features = {'image_raw': tf.FixedLenFeature([], tf.string),
-    #                     "label": tf.FixedLenFeature([], tf.int64)}
-    h=32
-    w=32
-    c=3
+  def parser_v1(self,record,h=32,w=32,c=3,num_classes=10):
     dtype=tf.float32
     features = {'image': tf.FixedLenFeature([h * w * c], dtype),
                  'label': tf.FixedLenFeature([], tf.int64)}
-    # example = tf.parse_single_example(proto, features=features)
-    example = tf.io.parse_single_example(proto, features=features)
-    # parsed_image = tf.io.decode_raw(example["image_raw"], tf.uint8)
-    # parsed_image = tf.cast(parsed_image, tf.float32)
-
-# parsed = tf.io.parse_single_example(record, keys_to_features)
-#     parsed_image = tf.io.decode_raw(parsed["image"], tf.uint8)
-#     parsed_image = tf.cast(parsed_image, tf.float32)
-#     parsed_image=  tf.reshape(parsed_image, [ parsed["height"], parsed["width"], parsed["depth"]])    
-#     label = tf.cast(parsed["label"], tf.int32)
-
-
-    # image = tf.decode_raw(example['image_raw'], tf.float32)
-    # image = tf.cast(image, tf.float32)
-
-    # image = tf.image.decode_jpeg(example['image_raw'], channels=3)
-    # image = tf.cast(example, tf.float32)
+    example = tf.io.parse_single_example(record, features=features)
     x, y = example["image"], example['label']
-    # x = tf.io.decode_raw(x, tf.uint8)
     # x = x / 255.0
     x = tf.reshape(x, [h, w, c])
     y = tf.one_hot(tf.cast(y, tf.int32), num_classes)
-    print(type(x),'----',x,'---------',y)
-
-    # # Load one example
-    # parsed_features = tf.parse_single_example(proto, keys_to_features)
-    
-    # # Turn your saved image string into an array
-    # parsed_features['image_raw'] = tf.decode_raw(
-    #     parsed_features['image_raw'], tf.uint8)
-    
-    # return parsed_features['image_raw'], parsed_features["label"]
     return x, y
 
   def parser_v2(self,record,h=32,w=32,c=3,num_classes=10):
@@ -139,7 +96,6 @@ class TFRecord:
     parsed = tf.io.parse_single_example(record, keys_to_features)
     parsed_image = tf.io.decode_raw(parsed["image"], tf.uint8)
     parsed_image = tf.cast(parsed_image, tf.float32) / 255.0
-    # parsed_image =  tf.reshape(parsed_image, [parsed["height"], parsed["width"], parsed["depth"]])    
     parsed_image =  tf.reshape(parsed_image, [h, w, c])
     # parsed_image = tf.image.per_image_standardization(parsed_image) ????? per image standarization -- should we use or not
     
