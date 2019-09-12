95f843fb27a6e38a9549079419f49d093cc1b546
satish4github <sathishkumar0017@gmail.com>
2019-09-12 09:19:50 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os.path
 from os import path
 from google.colab import drive
+from google.colab import files
 
 def is_file_exists(file_path):
       return path.exists(file_path)
@@ -10,3 +11,7 @@ def mkdirs(filepath):
               os.makedirs(filepath)
 def mount():
     drive.mount('/content/drive')
+
+def downloadModelPlot(file_name= 'model_plot.png'):
+      tf.keras.utils.plot_model(model, to_file=file_name, show_shapes=True, show_layer_names=True)
+      files.download(file_name)
